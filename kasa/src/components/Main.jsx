f66ec173aba9a9c927b4df13fd62f69f8f8cf9f7
boulyou tijari <tijarim7l@gmail.com>
2025-02-23 20:55:47 +0100

    Correction de titre

--- kasa/src/components/Main.jsx
+++ kasa/src/components/Main.jsx
@@ -2,22 +2,22 @@ function Main(){
   return(
     <div className="main__container">
       <div className="main__box">
-        <div className="main__title">Ritre de la location1</div>
+        <div className="main__title">Titre de la location1</div>
         </div>
         <div className="main__box">
-        <div className="main__title">Ritre de la location2</div>
+        <div className="main__title">Titre de la location2</div>
         </div>
         <div className="main__box">
-        <div className="main__title">Ritre de la location3</div>
+        <div className="main__title">Titre de la location3</div>
         </div>
         <div className="main__box">
-        <div className="main__title">Ritre de la location4</div>
+        <div className="main__title">Titre de la location4</div>
         </div>
         <div className="main__box">
-        <div className="main__title">Ritre de la location5</div>
+        <div className="main__title">Titre de la location5</div>
         </div>
         <div className="main__box">
-        <div className="main__title">Ritre de la location6</div>
+        <div className="main__title">Titre de la location6</div>
         </div>
     </div>
   )
